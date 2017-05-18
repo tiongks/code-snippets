bc9ff9dab99a80faa99d923cbd801cec6cc55194
Eric Tiongson <eric.tiongson@gmail.com>
2017-05-18 18:54:32 +0800

    updated to jdk 8u131

--- how-to-download-oracle-jdk.sh
+++ how-to-download-oracle-jdk.sh
@@ -6,7 +6,7 @@
 # 3. Right click the package link, copy link address: http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jdk-8u40-linux-x64.rpm
 # 4. Then, wget
 
-wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=access-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jdk-8u40-linux-x64.rpm
+wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=access-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
 
 
 
